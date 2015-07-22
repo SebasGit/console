855ba80d62899e2f51946d2054eb4ba39487206f
SebasGit <sraymond@dotloop.com>
2015-07-22 13:12:54 -0400

    Updated front end to look less 90s

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
-    collection.find({}, {sort: {_id: -1}, limit: 1}, function(err, records) {
+    collection.find({type: "regressionsheet"}, {sort: {_id: -1}, limit: 4}, function(err, records) {
         res.render('index', { title: 'QA Windows Console', "records" : records});
     });
 });
